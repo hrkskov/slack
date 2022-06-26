8acc9c6f1ff7ea85eda7b4b76891d476de97bda8
yuriskov <harukiti317@gmail.com>
2022-06-26 21:03:35 +0900

    Fix: Include all metadata

--- examples/metadata/metadata.go
+++ examples/metadata/metadata.go
@@ -11,16 +11,17 @@ func main() {
 	channelID, _, err := api.PostMessage(
 		"CHANNEL_ID",
 		slack.MsgOptionText("Some text", false),
-		slack.MsgOptionMetadata(slack.Metadata{EventType: "some_event_type", EventPayload: map[string]string{"some_event_payload": "some body"}}),
+		slack.MsgOptionMetadata(slack.Metadata{EventType: "some_event_type", EventPayload: map[string]string{"payload": "some body"}}),
 	)
 	if err != nil {
 		fmt.Printf("%s\n", err)
 		return
 	}
 
-	conversationHistory, err := api.GetConversationHistory(&slack.GetConversationHistoryParameters{ChannelID: channelID})
+	conversationHistory, err := api.GetConversationHistory(&slack.GetConversationHistoryParameters{ChannelID: channelID, IncludeAllMetadata: true})
 	if err != nil {
-		fmt.Printf("Got metadata: %s\n", conversationHistory.Messages[0].Metadata)
+		fmt.Printf("%s\n", err)
 		return
 	}
+	fmt.Printf("Successfully got metadata: %s\n", conversationHistory.Messages[0].Metadata)
 }
