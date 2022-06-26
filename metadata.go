285a6681a2c6e364751325d3853a7d513ced80fa
yuriskov <harukiti317@gmail.com>
2022-06-26 18:45:58 +0900

    Fix: Type of EventPayload

--- metadata.go
+++ metadata.go
@@ -2,6 +2,6 @@ package slack
 
 // Metadata contains metadata information
 type Metadata struct {
-	EventType    string `json:"event_type,omitempty"`
-	EventPayload string `json:"event_payload,omitempty"`
+	EventType    string `json:"event_type"`
+	EventPayload map[string]string `json:"event_payload"`
 }
